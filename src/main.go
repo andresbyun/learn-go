74b604afac69fdd110954c3e3203d992050c606b
Andres Byun <adb0202@hotmail.com>
2023-07-05 12:07:47 -0700

    Implement Vec3 to color conversion

--- src/main.go
+++ src/main.go
@@ -3,13 +3,26 @@ package main
 import (
 	"fmt"
 	"learn-go/util"
+	linAlgebra "learn-go/util/lin-algebra"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Set the image's height and width
+	fmt.Println("Initializing!")
 	Height := 400
 	Width := 400
 
+	// Initialize the 2D array that'll contain the image values
+	imgMat := make([][]linAlgebra.Vec3, Height)
+	for i := 0; i < Height; i++ {
+		imgMat[i] = make([]linAlgebra.Vec3, Width)
+		// Fill the background with white
+		for j := 0; j < Width; j++ {
+			imgMat[i][j] = linAlgebra.Vec3{X: 1, Y: 1, Z: 1}
+		}
+	}
+
+	// Generate the output image
 	output := util.Img{Width: Width, Height: Height, Name: "test.png"}
-	output.GenerateImage()
+	output.GenerateImage(imgMat)
 }
--- src/util/img.go
+++ src/util/img.go
@@ -5,6 +5,7 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	linAlgebra "learn-go/util/lin-algebra"
 	"os"
 	"strings"
 )
@@ -14,16 +15,16 @@ type Img struct {
 	Name          string
 }
 
-func (this Img) GenerateImage() {
+func (this Img) GenerateImage(colorMat [][]linAlgebra.Vec3) {
 	topLeft := image.Point{0, 0}
 	bottomRight := image.Point{this.Width, this.Height}
 	outputImg := image.NewRGBA(image.Rectangle{topLeft, bottomRight})
 
 	// Set color for each pixel.
-	for x := 0; x < this.Width; x++ {
-		for y := 0; y < this.Height; y++ {
-			// TODO: Convert values in matrix to color
-			outputImg.Set(x, y, color.White)
+	for i := 0; i < this.Height; i++ {
+		for j := 0; j < this.Width; j++ {
+			curr := Vec3ToRGBA(colorMat[i][j])
+			outputImg.Set(j, i, curr)
 		}
 	}
 
@@ -39,4 +40,15 @@ func (this Img) GenerateImage() {
 	f, _ := os.Create(this.Name)
 	png.Encode(f, outputImg)
 	fmt.Println("Created image: " + filename)
+}
+
+// Convert Vec3 to RGBA
+func Vec3ToRGBA(v linAlgebra.Vec3) color.RGBA {
+	uColor := map[uint8]uint8{
+		'r': uint8(v.X * 0xFF),
+		'g': uint8(v.Y * 0xFF),
+		'b': uint8(v.Z * 0xFF),
+	}
+
+	return color.RGBA{uColor['r'], uColor['g'], uColor['b'], 0xFF}
 }
